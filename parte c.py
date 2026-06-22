6b12f3f5c52b9166e8f1fac3f7734a33600ab1c4
renatacanonico1 <r.canonico@colegiocalasanz.edu.ar>
2026-06-22 11:33:35 -0300

    Add files via upload

---
+++ parte c.py
@@ -0,0 +1,18 @@
+# ===== PARTE C =====
+def porcentaje_vida(actual, maxima):    
+   porc= actual / maxima *100
+   return porcentaje
+def estado_vida(porcentaje):
+    if porc<=25:
+        return "critico"
+    elif porc<= 50:
+        return "herido"
+    else:
+        return "Sano"
+   def comprar_pociones(monedas, precio):
+     cantidad= monedas//precio
+     vuelto= monedas%precio
+     return cantidad,vuelto 
+
+
+       
