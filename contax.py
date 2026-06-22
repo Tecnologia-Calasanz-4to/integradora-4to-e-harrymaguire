801c0dfcd8c15f26eae7c67abc86af16201a08b0
lukiireyy <lukiireyy@gmail.com>
2026-06-22 11:29:23 -0300

    Add files via upload

---
+++ contax.py
@@ -0,0 +1,10 @@
+def nombre_valido(nombre):
+    if nombre>=2 and nombre.isalpha():
+    return True 
+def crear_codename(nombre, nivel)
+    nombre[0;3].upper()+str(nivel)
+    return codename
+def vida_maxima(nivel):
+    vida= 100+nivel**2*5
+    return vida 
+    
